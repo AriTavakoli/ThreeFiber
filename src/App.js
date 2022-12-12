3e9c6df59e75e4dfd69ef3dc001253c9ad9a38a1
AriTavakoli <aritavakoli98@gmail.com>
2022-12-12 14:03:37 -0800

    Animation init

--- src/App.js
+++ src/App.js
@@ -5,198 +5,51 @@ import { TransformControls, Environment, OrbitControls } from "@react-three/drei
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { Suspense } from "react";
 import { useGLTF } from '@react-three/drei'
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import { Perf } from 'r3f-perf'
-import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
-import { getProject } from '@theatre/core'
+import { getProject, core } from '@theatre/core'
 import { editable as e, SheetProvider } from '@theatre/r3f'
+import { useFrame } from '@react-three/fiber'
+import state from '@src/Animation/state2.json'
+import { PerspectiveCamera } from '@react-three/fiber'
 
 
+import studio from '@theatre/studio'
 
-studio.initialize()
-studio.extend(extension)
+const mode = 'development1'
+//
+if (mode === 'development') {
 
-const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
+  studio.extend(extension)
+} else {
 
-
-export function Model(props) {
-  const { nodes, materials } = useGLTF("models/gltf/car.glb");
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Text.geometry}
-        material={materials["temir]"]}
-        position={[0.07, 0.98, -2.82]}
-        rotation={[1.74, 0, -Math.PI]}
-        scale={0.08}
-      />
-      <group
-        position={[1.05, 0.43, 2.11]}
-        rotation={[0, -Math.PI / 2, 0]}
-        scale={[0.74, 0.75, 0.75]}
-      >
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane015_1.geometry}
-          material={materials["Material.002"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane015_2.geometry}
-          material={materials["Material.003"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane015_3.geometry}
-          material={materials["temir]"]}
-        />
-      </group>
-      <group
-        position={[1.05, 0.45, -1.57]}
-        rotation={[0, -Math.PI / 2, 0]}
-        scale={[0.74, 0.75, 0.75]}
-      >
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane000_1.geometry}
-          material={materials["Material.002"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane000_2.geometry}
-          material={materials["Material.003"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane000_3.geometry}
-          material={materials["temir]"]}
-        />
-      </group>
-      <group
-        position={[-1.02, 0.45, -1.57]}
-        rotation={[0, 1.57, 0]}
-        scale={[0.74, 0.75, 0.75]}
-      >
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane001.geometry}
-          material={materials["Material.002"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane001_1.geometry}
-          material={materials["Material.003"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane001_2.geometry}
-          material={materials["temir]"]}
-        />
-      </group>
-      <group
-        position={[-1.02, 0.43, 2.11]}
-        rotation={[0, 1.57, 0]}
-        scale={[0.74, 0.75, 0.75]}
-      >
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane002_1.geometry}
-          material={materials["Material.002"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane002_2.geometry}
-          material={materials["Material.003"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane002_3.geometry}
-          material={materials["temir]"]}
-        />
-      </group>
-      <group
-        position={[0.02, 1, 0.82]}
-        rotation={[0, -Math.PI / 2, 0]}
-        scale={[0.74, 0.75, 0.75]}
-      >
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane003_1.geometry}
-          material={materials.Material}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane003_2.geometry}
-          material={materials["temir]"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane003_3.geometry}
-          material={materials["Material.002"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane003_4.geometry}
-          material={materials.nomer}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane003_5.geometry}
-          material={materials.tereze}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane003_6.geometry}
-          material={materials.farb}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Plane003_7.geometry}
-          material={materials["Material.005"]}
-        />
-      </group>
-    </group>
-  );
 }
+studio.initialize()
+
+const demoSheet = getProject('Demo Project', { state: state }).sheet('Demo Sheet')
+const practiceAnimation = getProject('Animation').sheet('Practice Animation')
 
 
-useGLTF.preload("models/gltf/car.glb");
 
 
 
-function MyLight() {
-  // Get the transform controller for the light
-  const transform = useTransform()
+
+
+function Box(props) {
+  const boxRef = useRef()
+
+  useFrame(() => {
+    boxRef.current.rotation.x += 0.01
+    boxRef.current.rotation.y += 0.01
+  })
 
   return (
-    <directionalLight
-      // Set the transform controller as the light's parent
-      parent={transform}
-      position={[0, 0, 5]}
-      intensity={1}
-    />
+    <mesh {...props} ref={boxRef} castShadow receiveShadow>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color="hotpink" />
+    </mesh>
+
   )
 }
 
@@ -205,6 +58,10 @@ function App() {
 
   const lightRef = useRef()
   const boxRef = useRef()
+
+  useEffect(() => {
+    demoSheet.project.ready.then(() => demoSheet.sequence.play({ iterationCount: Infinity, range: [0, 5] }))
+  }, [])
 
 
 
@@ -222,10 +79,23 @@ function App() {
 
       <Perf position="bottom-left" />
 
-      <SheetProvider sheet={getProject('Demo Project').sheet('Demo Sheet')}>
+      <SheetProvider sheet={demoSheet}>
         <ambientLight />
         <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
-        <e.Model theatreKey="Car" position={[0, 0, 0]} />
+
+        <Box onClick = {() => {
+          demoSheet.sequence.play({ iterationCount: Infinity, range: [0, 5] })
+        }}></Box>
+
+        <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} color='black'>
+          <planeBufferGeometry args={[1, 1, 1]} color='black' />
+          <meshStandardMaterial color='black' />
+        </mesh>
+
+
+
+
+        {/* <e.Model theatreKey="Car" position={[0, 0, 0]} /> */}
         <e.mesh theatreKey="Cube">
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color="orange" />
---
+++ src/components/Model.jsx
@@ -0,0 +1,163 @@
+export function Model(props) {
+  const { nodes, materials } = useGLTF("models/gltf/car.glb");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Text.geometry}
+        material={materials["temir]"]}
+        position={[0.07, 0.98, -2.82]}
+        rotation={[1.74, 0, -Math.PI]}
+        scale={0.08}
+      />
+      <group
+        position={[1.05, 0.43, 2.11]}
+        rotation={[0, -Math.PI / 2, 0]}
+        scale={[0.74, 0.75, 0.75]}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane015_1.geometry}
+          material={materials["Material.002"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane015_2.geometry}
+          material={materials["Material.003"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane015_3.geometry}
+          material={materials["temir]"]}
+        />
+      </group>
+      <group
+        position={[1.05, 0.45, -1.57]}
+        rotation={[0, -Math.PI / 2, 0]}
+        scale={[0.74, 0.75, 0.75]}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane000_1.geometry}
+          material={materials["Material.002"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane000_2.geometry}
+          material={materials["Material.003"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane000_3.geometry}
+          material={materials["temir]"]}
+        />
+      </group>
+      <group
+        position={[-1.02, 0.45, -1.57]}
+        rotation={[0, 1.57, 0]}
+        scale={[0.74, 0.75, 0.75]}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane001.geometry}
+          material={materials["Material.002"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane001_1.geometry}
+          material={materials["Material.003"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane001_2.geometry}
+          material={materials["temir]"]}
+        />
+      </group>
+      <group
+        position={[-1.02, 0.43, 2.11]}
+        rotation={[0, 1.57, 0]}
+        scale={[0.74, 0.75, 0.75]}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane002_1.geometry}
+          material={materials["Material.002"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane002_2.geometry}
+          material={materials["Material.003"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane002_3.geometry}
+          material={materials["temir]"]}
+        />
+      </group>
+      <group
+        position={[0.02, 1, 0.82]}
+        rotation={[0, -Math.PI / 2, 0]}
+        scale={[0.74, 0.75, 0.75]}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane003_1.geometry}
+          material={materials.Material}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane003_2.geometry}
+          material={materials["temir]"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane003_3.geometry}
+          material={materials["Material.002"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane003_4.geometry}
+          material={materials.nomer}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane003_5.geometry}
+          material={materials.tereze}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane003_6.geometry}
+          material={materials.farb}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane003_7.geometry}
+          material={materials["Material.005"]}
+        />
+      </group>
+    </group>
+  );
+}
+
+
+useGLTF.preload("models/gltf/car.glb");
